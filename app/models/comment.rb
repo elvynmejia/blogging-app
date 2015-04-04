c1c08319d1b0507c42263e70e8de89022f2cd0a7
elvynmejia <elvynmejia@gmail.com>
2015-04-03 20:21:08 -0700

    Add associations between Posts and Comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+	#this model has the foreign key 
+	#belongs to some post
+	#belongs_to: posts
+	belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	#has many comments 
+	#has_many: comments 
+	has_many :comments, dependent: :destroy
 end
